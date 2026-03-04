1e8802de3b9ec2dba15ab630468e9aa2b886af2d
Hope <eilleeilyudah@gmail.com>
2026-03-03 22:47:21 -0500

    started dev subpage

---
+++ prettygourlave.nekoweb.org/dev/script.js
@@ -0,0 +1,8 @@
+function toggle_flicker() {
+  var x = document.getElementById("interlaced");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
